3baf95aa2eebded6595ba0ca3964b94f83019fb7
MuttonString <16234921893@qq.com>
2025-05-22 00:55:51 +0800

    体验优化

--- front-end/src/layout/index.tsx
+++ front-end/src/layout/index.tsx
@@ -254,6 +254,7 @@ export default function PageLayout() {
         ),
         key: 'submission',
         onClick() {
+          setMobileMenuOpen(false);
           navigate('submission');
         },
       },
